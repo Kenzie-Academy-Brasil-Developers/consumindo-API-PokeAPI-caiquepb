b2c618b24403d7593504ff94158ce86849e0e286
Caique Batista <caiquepbatistaa@gmail.com>
2023-01-01 21:44:00 -0300

    Entrega Realizada v1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,54 @@
-/* Monte sua lógica aqui */
+async function renderizaPokemons() {
+    const ulTag = document.querySelector('ul')
+
+    const listaDePokemons = await consomePokeAPI()
+
+    listaDePokemons.results.forEach(pokemon => {
+        
+        const numeroNaPokedex = pokemon.url.slice(34, -1)
+
+        ulTag.insertAdjacentHTML('beforeend', `
+            <li>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png" alt=${pokemon.name}>
+                <h3>${pokemon.name}</h3>
+            </li>
+        `)
+    })
+}
+
+function renderSearch(){
+
+    const searchInput = document.querySelector('.search__input')
+    const searchIcon = document.querySelector('.search__icon')
+    const mainList = document.querySelector('.main__list')
+
+    searchIcon.addEventListener('click', () => {
+
+        mainList.innerHTML = '' 
+
+        pokemonSearch(searchInput.value.toLowerCase())
+
+    })
+
+}
+
+function renderAll(){
+
+    const pokemonLogo = document.querySelector('.main__logo')
+    const mainList = document.querySelector('.main__list')
+
+    pokemonLogo.addEventListener('click', () => {
+
+        mainList.innerHTML = '' 
+
+        renderizaPokemons()
+
+    })
+
+}
+
+renderizaPokemons()
+
+renderSearch()
+
+renderAll()
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,50 @@
-/* Monte a lógica das requisições aqui */
+async function consomePokeAPI() {
+
+    const loading = document.querySelector('#loading')
+
+    const pokemonsDaAPI = await fetch('https://pokeapi.co/api/v2/pokemon', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+      .then(
+        response => response.json()
+      )
+
+      .catch(
+        error => console.log(error)
+      )
+
+    loading.classList.add('hidden')
+
+    return pokemonsDaAPI
+}
+
+async function pokemonSearch(pokemonName){
+
+  const mainList = document.querySelector('.main__list')
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  .then(response => response.json())
+
+  .then(pokemon => {
+
+    mainList.insertAdjacentHTML('beforeend', `
+            <li>
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt=${pokemon.name}><h3>${pokemon.name}</h3>
+            </li>
+        `)
+  })
+
+  return pokemon
+
+}
+
+consomePokeAPI()
